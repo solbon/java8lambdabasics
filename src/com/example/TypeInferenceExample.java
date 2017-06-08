0da72a5f8ef38247e0d7e38743bb7cc31b1400c6
Solbon from Kizhinga <solbon@gmail.com>
2017-06-08 14:21:27 +0800

    Type inference

---
+++ src/com/example/TypeInferenceExample.java
@@ -0,0 +1,20 @@
+package com.example;
+
+/**
+ * Created by Гунга on 08.06.2017.
+ */
+public class TypeInferenceExample {
+
+    public static void main(String[] args) {
+        printLambda(s -> s.length());
+    }
+
+    public static void printLambda(StringLengthLambda l) {
+        System.out.println(l.getLength("Hello Lambda"));
+    }
+
+    interface StringLengthLambda {
+        int getLength(String s);
+    }
+
+}
