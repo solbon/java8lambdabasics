9d58c44a6534c3009028aceee807eea78a06b512
Solbon from Kizhinga <solbon@gmail.com>
2017-06-08 23:51:21 +0800

    Exception handling approach in another lambda

--- src/com/example/unit2/ExceptionHandlingExample.java
+++ src/com/example/unit2/ExceptionHandlingExample.java
@@ -9,19 +9,22 @@ public class ExceptionHandlingExample {
     public static void main(String[] args) {
         int[] numbers = {1, 2, 3, 4};
         int key = 0;
-        process(numbers, key, (v, k) -> {
-            try {
-                System.out.println(v / k);
-            } catch (ArithmeticException ex) {
-                System.out.println("An arithmetic exception handled");
-            }
-        });
-
+        process(numbers, key, wrappedLambda((v, k) -> System.out.println(v / k)));
     }
 
     private static void process(int[] numbers, int key, BiConsumer<Integer, Integer> consumer) {
         for (int i : numbers) {
             consumer.accept(i, key);
         }
+    }
+
+    private static BiConsumer<Integer, Integer> wrappedLambda(BiConsumer<Integer, Integer> consumer) {
+        return (v, k) -> {
+            try {
+                consumer.accept(v, k);
+            } catch (ArithmeticException ex) {
+                System.out.println("Arithmetic exception caught in wrapped lambda");
+            }
+        };
     }
 }
