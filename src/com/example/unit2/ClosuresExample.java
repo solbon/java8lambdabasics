2fe8e33a11e993952d20097c8e94aff670cf38b6
Solbon Daribazaron <solbon@gmail.com>
2017-06-19 16:37:48 +0200

    Closures in Lambda expressions

---
+++ src/com/example/unit2/ClosuresExample.java
@@ -0,0 +1,20 @@
+package com.example.unit2;
+
+/**
+ * Created by Solbon on 2017-06-19.
+ */
+public class ClosuresExample {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        doProcess(a, i -> System.out.println(i + b));
+    }
+
+    public static void doProcess(int i, Process p) {
+        p.process(i);
+    }
+}
+
+interface Process {
+    void process(int i);
+}
